b84d6c5c1f59c2bd0b72e465550c5fa5b0e925eb
TareqAbughosh <30448064+TareqAbughosh@users.noreply.github.com>
2023-09-23 17:06:01 +0300

    Added api for mailtrap

--- app/Http/Controllers/ContactUsController.php
+++ app/Http/Controllers/ContactUsController.php
@@ -63,6 +63,7 @@ class ContactUsController extends Controller
             ");
          });
         } catch (\Exception $e){
+            dd($e->getMessage());
             return back()->with('error', 'sorry something went wrong!');
         }
         return back()->with('success', 'Your form has been submitted successfully!');
