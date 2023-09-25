5071af86e1737d4c6f86700176e866713d8afaf3
TareqAbughosh <30448064+TareqAbughosh@users.noreply.github.com>
2023-09-26 01:10:47 +0300

    first commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -67,6 +67,12 @@
   transform: translateY(-5px);
   transition: all 0.3s;
 }
+.animatepls {
+    background-color: whitesmoke;
+  transform: translateY(-5px);
+  transition: all 0.3s;
+  box-shadow: 0px 20px 35px -16px rgba(255, 159, 16, 0.6);
+}
 </style>
 @section('content')
 <section class="page_slider">
@@ -162,26 +168,52 @@
             <div class="col-lg-6">
                 <div class="divider-40 hidden-above-lg"></div>
                 <div class="button-container">
-  
-                    <div class="glass-btn amber-btn">
-                      <!--<div class="content">
-                        h
-                      </div> -->
-                      <i class="fa fa-book fa-2x"></i>
-                    </div>
-                  
-                    <div class="glass-btn red-btn">
-                      <!-- <div class="content">
-                        e
-                      </div> -->
-                      <img src="https://i.postimg.cc/LstJ4Hhf/youtube.png" alt="youtube" style="width: 6em;">
-                    </div>
-                    
-                    <div class="glass-btn amber-btn">
-                      <!-- <div class="content">
-                        y
-                      </div> -->
-                      <img src="https://i.postimg.cc/tgQ1H1Rp/soundcloud.png" alt="soundcloud" style="width: 6em;">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="card border-0">
+                                <div class="glass-btn amber-btn">
+                                    <i class="fas fa-chart-bar fa-2x"></i>
+                                  </div>
+                                <div class="card-body">
+                                    <h5 class="card-title">Trading Opportunities</h5>
+                                    <p class="card-text">Identify & optimize trading opportunities for value creation.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="card border-0">
+                                <div class="glass-btn amber-btn">
+                                    <i class="fa fa-book fa-2x"></i>
+                                  </div>
+                                <div class="card-body">
+                                    <h5 class="card-title">Business Services</h5>
+                                    <p class="card-text">Use deep trading skills to assist counter-parties for commercially prudent decision-making with minimum risks.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="card border-0">
+                                <div class="glass-btn amber-btn">
+                                    <i class="fa fa-handshake fa-2x"></i>
+                                  </div>
+                                <div class="card-body">
+                                    <h5 class="card-title">
+                                        Networks & Partnerships</h5>
+                                    <p class="card-text">Leverage network & partnerships to efficiently facilitate flow of oil commodities across market place.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="card border-0">
+                                <div class="glass-btn amber-btn">
+                                    <i class="fa fa-tasks fa-2x"></i>
+                                  </div>
+                                <div class="card-body">
+                                    <h5 class="card-title">Transparency</h5>
+                                    <p class="card-text">Build transparency & ethicality in operations to grow trust & partnership.</p>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                   
                   </div>
@@ -415,143 +447,22 @@
         </div>
     </div>
 </section>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+<script>
+$('.card').mouseenter(function() {
+    // Find the child element with the class "child-class" within the parent div
+    const childElement = $(this).children().filter('.glass-btn');
+    // Add an animation class to the child element
+    childElement.addClass('animatepls');
+});
 
-{{-- <section class="ls ms s-pt-xl-160 s-pt-lg-130 s-pt-md-90 s-pt-60 s-pb-xl-280 s-pb-lg-250 s-pb-md-90 s-pb-60">
-    <div class="container">
-        <div class="row">
-            <div class="col-12">
-                <h2 class="special-heading text-center">
-                    <span class="text-capitalize">
-                        Latest News
-                    </span>
-                </h2>
-                <p class="special-heading text-center">
-                    <span class="text-capitalize color-darkgrey bold">
-                        our blog
-                    </span>
-                </p>
-                <div class="divider-50 hidden-below-lg"></div>
-                <div class="divider-30 hidden-above-lg"></div>
-                <div class="shortcode-posts row tiled-layout c-gutter-30 c-mb-30">
-                    <div class="bigitem col-xl-6 col-lg-5">
-                        <article class="vertical-item ls rounded post type-post status-publish format-standard has-post-thumbnail">
-                            <div class="item-media post-thumbnail">
-                                <a href="blog-single-@@type.html">
-                                    <img src="/images/blog/01.jpg" alt="img">
-                                </a>
-                            </div><!-- .post-thumbnail -->
-                            <div class="item-content">
-                                <header class="entry-header">
-                                    <div class="entry-meta">
-                                        <div class="byline">
-                                            <span class="date">
-                                                <a href="blog-@@type.html" rel="bookmark">
-                                                    <time class="published entry-date" datetime="2019-04-09T12:23:09+00:00">20.03.2019</time>
-                                                </a>
-                                            </span>
-                                            <span class="author vcard">
-                                                <a class="url fn n" href="blog-@@type.html" rel="author"><span>by</span> Admin</a>
-                                            </span>
-                                        </div>
-                                    </div>
-                                    <h4 class="entry-title">
-                                        <a href="blog-single-@@type.html" rel="bookmark">
-                                            Consectetur adipisicing elited do eiusmod tempor incididunt ut labore.
-                                        </a>
-                                    </h4>
-
-                                    <!-- .entry-meta -->
-                                </header>
-                                <!-- .entry-header -->
-
-                                <div class="entry-content">
-                                    <p>
-                                        Tempor incididunt labore dolmagna aliqua eniminim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate.
-                                    </p>
-                                </div><!-- .entry-content -->
-                            </div><!-- .item-content -->
-                        </article><!-- #post-## -->
-                    </div>
-                    <div class=" smallitem col-xl-6 col-lg-7">
-                        <article class="vertical-item side-item ls rounded post type-post status-publish format-standard has-post-thumbnail">
-                            <div class="item-media post-thumbnail">
-                                <a href="blog-single-@@type.html">
-                                    <img src="/images/blog/square/02.jpg" alt="img">
-                                </a>
-                            </div><!-- .post-thumbnail -->
-                            <div class="item-content">
-                                <header class="entry-header">
-                                    <div class="entry-meta">
-                                        <div class="byline">
-                                            <span class="date">
-                                                <a href="blog-@@type.html" rel="bookmark">
-                                                    <time class="published entry-date" datetime="2019-04-09T12:23:09+00:00">20.03.2019</time>
-                                                </a>
-                                            </span>
-                                            <span class="author vcard">
-                                                <a class="url fn n" href="blog-@@type.html" rel="author"><span>by</span> Admin</a>
-                                            </span>
-                                        </div>
-                                    </div>
-                                    <h4 class="entry-title">
-                                        <a href="blog-single-@@type.html" rel="bookmark">
-                                            Lorem ipsum dolor sit amet consectetur
-                                        </a>
-                                    </h4>
-
-                                    <!-- .entry-meta -->
-                                </header>
-                                <!-- .entry-header -->
-
-                                <div class="entry-content">
-                                    <p>
-                                        Tempor incididunt labore dolmagna aliqua eniminim veniam quis.
-                                    </p>
-                                </div><!-- .entry-content -->
-                            </div><!-- .item-content -->
-                        </article>
-                        <article class="vertical-item side-item ls rounded post type-post status-publish format-standard has-post-thumbnail">
-                            <div class="item-media post-thumbnail">
-                                <a href="blog-single-@@type.html">
-                                    <img src="/images/blog/square/03.jpg" alt="img">
-                                </a>
-                            </div><!-- .post-thumbnail -->
-                            <div class="item-content">
-                                <header class="entry-header">
-                                    <div class="entry-meta">
-                                        <div class="byline">
-                                            <span class="date">
-                                                <a href="blog-@@type.html" rel="bookmark">
-                                                    <time class="published entry-date" datetime="2019-04-09T12:23:09+00:00">20.03.2019</time>
-                                                </a>
-                                            </span>
-                                            <span class="author vcard">
-                                                <a class="url fn n" href="blog-@@type.html" rel="author"><span>by</span> Admin</a>
-                                            </span>
-                                        </div>
-                                    </div>
-                                    <h4 class="entry-title">
-                                        <a href="blog-single-@@type.html" rel="bookmark">
-                                            Adipisicing elit sed do eiusmod
-                                        </a>
-                                    </h4>
-
-                                    <!-- .entry-meta -->
-                                </header>
-                                <!-- .entry-header -->
-
-                                <div class="entry-content">
-                                    <p>
-                                        Nstrud exercitation ullamco laboris nisi ut aliquip ex ea commodo.
-                                    </p>
-                                </div><!-- .entry-content -->
-                            </div><!-- .item-content -->
-                        </article>
-                    </div>
-                </div>
-                <div class="mt--30"></div>
-            </div>
-        </div>
-    </div>
-</section> --}}
+// If you also want to remove the animation class when the mouse leaves the parent div
+$('.card').mouseleave(function() {
+    // Find the child element with the class "child-class" within the parent div
+    const childElement = $(this).find('.glass-btn');
+    
+    // Remove the animation class from the child element
+    childElement.removeClass('animatepls');
+});
+</script>
 @endsection
